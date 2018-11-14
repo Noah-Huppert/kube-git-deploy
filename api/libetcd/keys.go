3068f39d7ea74804cc8f807fd0ed68ed65d14da3
Noah Huppert <contact@noahh.io>
2018-11-14 13:10:13 -0500

    api: wrote Repository.Create method

--- api/libetcd/keys.go
+++ api/libetcd/keys.go
@@ -9,30 +9,3 @@ const KeyGitHubAuthToken string = "/github/auth/token"
 
 // KeyDirTrackedGHRepos is the key used to store tracked GitHub repositories
 const KeyDirTrackedGHRepos string = "/github/repositories/tracked"
-
-// KeyTrackedGHRepoName is the key placed inside of a GitHub repo directory
-// which stores its name
-const KeyTrackedGHRepoName string = "name"
-
-// KeyTrackedGHRepoWebHookID is the key placed inside of a GitHub repo
-// directory which stores its web hook ID
-const KeyTrackedGHRepoWebHookID string = "web_hook_id"
-
-// GetTrackedGHRepoDirKey returns the directory key for a tracked
-// GitHub repository
-func GetTrackedGHRepoDirKey(user, repo string) string {
-	return fmt.Sprintf("%s/%s/%s", KeyDirTrackedGHRepos, user, repo)
-}
-
-// GetTrackedGHRepoNameKey returns a key to a node which holds a repo's name
-func GetTrackedGHRepoNameKey(user, repo string) string {
-	return fmt.Sprintf("%s/%s", GetTrackedGHRepoDirKey(user, repo),
-		KeyTrackedGHRepoName)
-}
-
-// GetTrackedGHRepoWebHookIDKey returns a key to a node which holds a repo's
-// web hook ID
-func GetTrackedGHRepoWebHookIDKey(user, repo string) string {
-	return fmt.Sprintf("%s/%s", GetTrackedGHRepoDirKey(user, repo),
-		KeyTrackedGHRepoWebHookID)
-}
--- api/models/repo.go
+++ api/models/repo.go
@@ -21,10 +21,56 @@ type Repository struct {
 	WebHookID int `json:"web_hook_id"`
 }
 
+// GetTrackedGHRepoDirKey returns the directory key for a tracked
+// GitHub repository
+func GetTrackedGHRepoDirKey(user, repo string) string {
+	return fmt.Sprintf("%s/%s/%s", libetcd.KeyDirTrackedGHRepos, user,
+		repo)
+}
+
 // key returns the Etcd key the repository should be stored in
-func (r *Repository) key() string {
-	return fmt.Sprintf("%s/%s/%s/information", libetcd.KeyDirTrackedGHRepos, r.Owner,
-		r.Name)
+func (r Repository) key() string {
+	return fmt.Sprintf("%s/%s/information",
+		GetTrackedGHRepoDirKey(r.Owner, r.Name))
+}
+
+// Creates creates the directory structure for a Repository in Etcd and
+// Sets a directory. Does not work if Repository was previously saved.
+func (r Repository) Create(ctx context.Context, etcdKV etcd.KeysAPI) error {
+	// Create top level directory
+	dirName := GetTrackedGHRepoDirKey(repo.Owner, repo.Name)
+
+	_, err := etcdKV.Set(ctx, dirName, "", &etcd.SetOptions{
+		Dir:       true,
+		PrevExist: etcd.PrevNoExist,
+	})
+
+	if err != nil {
+		return fmt.Errorf("error creating repository directory: %s",
+			err.Error())
+	}
+
+	// Create jobs directory
+	jobsDirName := fmt.Sprintf("%s/jobs", dirName)
+
+	_, err = etcdKV.Set(ctx, jobsDirName, "", &etcd.SetOptions{
+		Dir:       true,
+		PrevExist: etcd.PrevNoExist,
+	})
+
+	if err != nil {
+		return fmt.Errorf("error creating jobs directory: %s",
+			err.Error())
+	}
+
+	// Save repository in directory
+	err = repo.Set(ctx, etcdKV)
+
+	if err != nil {
+		return fmt.Errorf("error saving repository: %s", err.Error())
+	}
+
+	return nil
 }
 
 // Set stores a repository in Etcd
